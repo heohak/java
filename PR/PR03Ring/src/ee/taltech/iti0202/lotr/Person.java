af01f0741a3130c7706fad2232e40e0529d8beb7
heohak <heohak@ttu.ee>
2023-02-15 12:57:46 +0200

    pr03

---
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
@@ -0,0 +1,53 @@
+package ee.taltech.iti0202.lotr;
+
+public class Person {
+
+
+    private String race;
+    private String name;
+    private Ring ring;
+
+    public Person(String race, String name, Ring ring) {
+
+        this.race = race;
+        this.name = name;
+        this.ring = ring;
+    }
+
+    public Person(String race, String name) {
+        this.race = race;
+        this.name = name;
+        this.ring = null;
+    }
+
+    public void setRing(Ring ring) {
+        this.ring = ring;
+    }
+
+    public String isSauron() {
+
+        if (getName().equals("Sauron") && getRing().getType().equals(Ring.Type.THE_ONE) && getRing().getMaterial().equals(Ring.Material.GOLD)) {
+            return "Affirmative";
+        } else if (getName().equals("Sauron") && getRing().getType().equals(Ring.Type.THE_ONE) && !(getRing().getMaterial().equals(Ring.Material.GOLD))) {
+            return "No, the ring is fake!";
+        } else if (!(getName().equals("Sauron")) && getRing().getType().equals(Ring.Type.THE_ONE) && getRing().getMaterial().equals(Ring.Material.GOLD)) {
+            return "No, he just stole the ring.";
+        } else if (getName().equals("Sauron") && !getRing().getType().equals(Ring.Type.THE_ONE)) {
+            return "No, but he's claiming to be.";
+        } else {
+            return "No";
+        }
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Ring getRing() {
+        return ring;
+    }
+}
