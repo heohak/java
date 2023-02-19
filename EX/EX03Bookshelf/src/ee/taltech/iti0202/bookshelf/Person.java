75b4d05ef0d17f15d6103df467fdab70657d276c
heohak <heohak@ttu.ee>
2023-02-19 22:12:55 +0200

    ex03

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Person.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Person.java
@@ -4,28 +4,54 @@ public class Person {
     private String name;
     private int money;
 
+    /**
+     *
+     * @param name
+     * @param money
+     */
     public Person(String name, int money) {
         this.name = name;
         this.money = money;
 
     }
 
+    /**
+     *
+     * @return int
+     */
     public int getMoney() {
         return this.money;
     }
 
+    /**
+     *
+     * @param sum
+     */
     public void addMoney(int sum) {
         this.money = this.money + sum;
     }
 
+    /**
+     *
+     * @param sum
+     */
     public void takeMoney(int sum) {
         this.money = this.money - sum;
     }
 
+    /**
+     *
+     * @return String
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     *
+     * @param book
+     * @return boolean
+     */
     public boolean buyBook(Book book) {
         if (book == null) {
             return false;
@@ -39,6 +65,11 @@ public class Person {
         }
     }
 
+    /**
+     *
+     * @param book
+     * @return boolean
+     */
     public boolean sellBook(Book book) {
         if (book == null || book.getOwner() == null) {
             return false;
