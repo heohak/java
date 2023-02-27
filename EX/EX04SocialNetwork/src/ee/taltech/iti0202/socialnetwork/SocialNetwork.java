6ea7a74a173d7029796a09cd793a9b4ab2652e62
heohak <heohak@ttu.ee>
2023-02-27 19:58:48 +0200

    ex04

---
+++ EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetwork.java
@@ -0,0 +1,31 @@
+package ee.taltech.iti0202.socialnetwork;
+import ee.taltech.iti0202.socialnetwork.feed.Feed;
+import ee.taltech.iti0202.socialnetwork.group.Group;
+import ee.taltech.iti0202.socialnetwork.message.Message;
+import ee.taltech.iti0202.socialnetwork.user.User;
+
+import java.util.*;
+
+public class SocialNetwork {
+    Set<Group> networkGroups = new HashSet<>();
+
+    public void registerGroup(Group group) {
+        networkGroups.add(group);
+
+    }
+
+    public Set<Group> getGroups() {
+        return networkGroups;
+    }
+
+    public Feed getFeedForUser(User user) {
+        Set<Message> feed1 = new HashSet<>();
+
+        for (Group group : networkGroups) {
+            if (group.getParticipants().contains(user)) {
+                feed1.addAll(group.getMessages());
+            }
+        }
+        return new Feed(user, feed1);
+    }
+}
