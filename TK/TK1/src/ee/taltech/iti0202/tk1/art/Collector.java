ca1301995be7b8eed7deead6c6963e60faa1bbe5
heohak <heohak@ttu.ee>
2023-03-01 16:57:03 +0200

    tk1

---
+++ TK/TK1/src/ee/taltech/iti0202/tk1/art/Collector.java
@@ -0,0 +1,35 @@
+package ee.taltech.iti0202.tk1.art;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collector {
+
+    private List<Painting> paintings = new ArrayList<>();
+
+    public boolean addPainting(Painting painting) {
+        if (!paintings.contains(painting)) {
+            for (Painting p : paintings) {
+                if (p.getTitle().equals(painting.getTitle())) {
+                    return false;
+                }
+            }
+            paintings.add(painting);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean sellPainting(Painting painting, Collector fellowCollector) {
+        if (paintings.contains(painting) && fellowCollector != this && fellowCollector.addPainting(painting)) {
+            paintings.remove(painting);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Painting> getPaintings() {
+        return paintings;
+    }
+}
---
+++ TK/TK1/src/ee/taltech/iti0202/tk1/art/Painting.java
@@ -0,0 +1,39 @@
+package ee.taltech.iti0202.tk1.art;
+
+public class Painting {
+    private String title;
+    private String author;
+
+    public Painting(String title) {
+        this.title = title;
+    }
+
+    public Painting(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String toString() {
+        if (author != null) {
+            return "This is a painting named " + title + " and made by " + author + ".";
+        } else {
+            return "This is a painting named " + title + " and made by an unknown artist.";
+        }
+    }
+}
