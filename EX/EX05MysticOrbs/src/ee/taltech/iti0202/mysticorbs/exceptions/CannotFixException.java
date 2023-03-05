9aab945c4a115148638faae0b082a24ac4ad0ee1
heohak <heohak@ttu.ee>
2023-03-05 23:35:55 +0200

    exfking05

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/exceptions/CannotFixException.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/exceptions/CannotFixException.java
@@ -13,11 +13,6 @@ public class CannotFixException extends Exception {
 
     }
 
-    /**
-     *
-     * @param oven
-     * @param reason
-     */
     public CannotFixException(Oven oven, Reason reason) {
         this.oven = oven;
         this.reason = reason;
