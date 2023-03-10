25529118bd80228c4fe024e44c9ee6a9717ff3ca
heohak <heohak@ttu.ee>
2023-03-10 17:24:01 +0200

    ex06

--- EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
@@ -1,27 +1,64 @@
 package ee.taltech.iti0202.files.morse;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MorseTranslator {
+    private Map<String, String> morseCodes = new HashMap<>();
 
     public Map<String, String> addMorseCodes(List<String> lines) {
-        return null;
+        for (String line : lines) {
+            String[] parts = line.split(" ");
+            morseCodes.put(parts[1].toLowerCase(), parts[0]);
+        }
+        return morseCodes;
     }
 
     public List<String> translateLinesToMorse(List<String> lines) {
-        return null;
+        List<String> result = new ArrayList<>();
+        for (String line : lines) {
+            String translatedLine = translateLineToMorse(line);
+            result.add(translatedLine);
+        }
+        return result;
     }
 
     public List<String> translateLinesFromMorse(List<String> lines) {
-        return null;
+        List<String> result = new ArrayList<>();
+        for (String line : lines) {
+            String translatedLine = translateLineFromMorse(line);
+            result.add(translatedLine);
+        }
+        return result;
     }
 
     private String translateLineToMorse(String line) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (char c : line.toLowerCase().toCharArray()) {
+            String morseCode = morseCodes.get(String.valueOf(c));
+            if (morseCode != null) {
+                sb.append(morseCode).append(" ");
+            }
+        }
+        return sb.toString().trim();
+
     }
 
     private String translateLineFromMorse(String line) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        String[] morseWords = line.split("\t");
+        for (String morseWord : morseWords) {
+            String[] morseletters = morseWord.split(" ");
+            for (String morseLetter : morseletters) {
+                String letter = morseCodes.get(morseLetter);
+                if (letter != null) {
+                    sb.append(letter);
+                }
+            }
+            sb.append(" ");
+        }
+        return sb.toString().trim();
     }
 }
--- EX/EX06Files/src/ee/taltech/iti0202/files/output/OutputFilesWriter.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/output/OutputFilesWriter.java
@@ -1,10 +1,26 @@
 package ee.taltech.iti0202.files.output;
 
+import javax.imageio.IIOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class OutputFilesWriter {
 
     public boolean writeLinesToFile(List<String> lines, String filename) {
-        return false;
+        if (filename == null) {
+            return false;
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
