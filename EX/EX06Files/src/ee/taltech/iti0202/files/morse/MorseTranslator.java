9e575b5aa7922ab2d7c15c0aee130689d59ac86c
heohak <heohak@ttu.ee>
2023-03-13 12:36:56 +0200

    ex06

--- EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
@@ -54,15 +54,18 @@ public class MorseTranslator {
      */
     private String translateLineToMorse(String line) {
         StringBuilder morseLine = new StringBuilder();
-        String[] result = line.split(" ");
-        for (String word : result) {
-            for (int i = 0; i < word.length(); i++) {
-                morseLine.append(morseCodes.get(String.valueOf(word.toLowerCase().charAt(i))));
-                morseLine.append(" ");
+        for (char c : line.toLowerCase().toCharArray()) {
+            if (c == ' ') {
+                morseLine.append("\t");
+            } else {
+                String morseCode = morseCodes.get(Character.toString(c));
+                if (morseCode != null) {
+                    morseLine.append(morseCode).append(" ");
+                }
             }
-            morseLine.append("\t");
         }
-    return morseLine.toString();
+        return morseLine.toString().trim();
+
     }
 
 
