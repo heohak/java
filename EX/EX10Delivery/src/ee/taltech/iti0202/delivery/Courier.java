7ded35a007b277752eac0133938244761f5f39a1
heohak <heohak@ttu.ee>
2023-04-06 01:42:15 +0300

    ex10

--- EX/EX10Delivery/src/ee/taltech/iti0202/delivery/Courier.java
+++ EX/EX10Delivery/src/ee/taltech/iti0202/delivery/Courier.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 class Courier {
     private String name;
     private Location currentLocation;
+    private Location destination;
+    private int remainingDistance;
     private Map<String, Packet> carriedPackets = new HashMap<>();
     private Strategy strategy;
 
@@ -27,7 +29,43 @@ class Courier {
         return strategy;
     }
 
+    public String getName() {
+        return name;
+    }
+
     void tick(Map<String, Location> locations) {
-        // Implement tick logic based on the requirements
+        if (destination == null) {
+            if (strategy != null) {
+                Action action = strategy.getAction();
+                if (action != null) {
+                    destination = action.getGoTo();
+                    remainingDistance = currentLocation.getDistanceTo(destination.getName());
+
+                    // Deposit packages
+                    for (String packetName : action.getDeposit()) {
+                        Packet packet = carriedPackets.remove(packetName);
+                        if (packet != null) {
+                            currentLocation.addPacket(packet);
+                        }
+                    }
+
+                    // Take packages
+                    for (String packetName : action.getTake()) {
+                        Optional<Packet> packetOpt = currentLocation.getPacket(packetName);
+                        if (packetOpt.isPresent()) {
+                            carriedPackets.put(packetName, packetOpt.get());
+                        }
+                    }
+                }
+            }
+        } else {
+            remainingDistance--;
+            if (remainingDistance == 0) {
+                currentLocation = destination;
+                destination = null;
+            } else {
+                currentLocation = null;
+            }
+        }
     }
 }
