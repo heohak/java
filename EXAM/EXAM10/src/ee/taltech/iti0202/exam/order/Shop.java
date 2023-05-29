4250412a68a6eeda127dbde2b4f8c5a14b6b884e
heohak <heohak@ttu.ee>
2023-05-29 23:34:33 +0300

    harjutamine

--- EXAM/EXAM10/src/ee/taltech/iti0202/exam/order/Shop.java
+++ EXAM/EXAM10/src/ee/taltech/iti0202/exam/order/Shop.java
@@ -1,7 +1,9 @@
 package ee.taltech.iti0202.exam.order;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class Shop {
@@ -34,6 +36,12 @@ public class Shop {
         return order.getId();
     }
 
+    public List<Product> SortByCheapest(List<Product> products) {
+        return products.stream()
+                .sorted(Comparator.comparing(Product::getPrice))
+                .collect(Collectors.toList());
+    }
+
     public boolean addProductToOrder(int orderNumber, String itemName) {
         for (Order order : contrabandList) {
             if (order.getId() == orderNumber) {
@@ -45,7 +53,7 @@ public class Shop {
         for (Order order : orders) {
             if (order.getId() == orderNumber) {
                 orderFound = true;
-                for (Product product : products) {
+                for (Product product : SortByCheapest(products)) {
                     if (product.getName().equals(itemName)) {
                         productFound = true;
                         order.getOrderProducts().add(product);
@@ -83,5 +91,45 @@ public class Shop {
 
     public List<Product> getAvailableProducts() {
         return products;
+    }
+
+
+    public static void main(String[] args) {
+
+        Shop topShop = new Shop();
+        Product p1 = new Product("TV", 100);
+        Product p2 = new Product("Radio", 60);
+        Product p3 = new Product("TV", 90);
+        topShop.addProduct(p1);
+        topShop.addProduct(p2);
+        topShop.addProduct(p3);
+
+        int orderNumber1 = topShop.createNewOrder();
+        System.out.println(orderNumber1); // 1
+        System.out.println(topShop.addProductToOrder(orderNumber1, "Radio")); // true
+        System.out.println(topShop.addProductToOrder(orderNumber1, "Cheese")); // false
+        System.out.println(topShop.addProductToOrder(orderNumber1, "Radio")); // false
+
+        Product p4 = new Product("TV", 100);
+        System.out.println(topShop.addProduct(p3)); // false - same product instance not allowed
+        System.out.println(topShop.addProduct(p4)); // true - another product with same values is allowed
+
+        int orderNumber2 = topShop.createNewOrder();
+        System.out.println(orderNumber2); // 2
+        System.out.println(topShop.addProductToOrder(orderNumber2, "TV")); // true
+        System.out.println(topShop.getOrderSum(orderNumber2)); // 90
+        System.out.println(topShop.addProductToOrder(orderNumber2, "TV")); // true
+        System.out.println(topShop.getOrderSum(orderNumber2)); // 190
+        System.out.println(topShop.addProductToOrder(orderNumber2, "TV")); // true
+        System.out.println(topShop.getOrderSum(orderNumber2)); // 290
+        System.out.println(topShop.addProductToOrder(orderNumber2, "TV")); // false
+        System.out.println(topShop.getOrderSum(orderNumber2)); // 290
+
+// cancel order
+        topShop.cancelOrder(orderNumber1); // free all the products from order 1
+        int orderNumber3 = topShop.createNewOrder();
+        System.out.println(orderNumber3); // 3
+        System.out.println(topShop.addProductToOrder(orderNumber3, "Radio")); // true
+        System.out.println(topShop.getOrderSum(orderNumber3)); // 60
     }
 }
