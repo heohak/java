5bbc5d8492397b213ee04ba9fb44a37cf9213b29
heohak <heohak@ttu.ee>
2023-05-29 15:12:38 +0300

    harjutamine

--- TK/TK3/src/ee/taltech/iti0202/tk/Exam.java
+++ TK/TK3/src/ee/taltech/iti0202/tk/Exam.java
@@ -60,7 +60,7 @@ public class Exam {
     /**
      * Given three ints, a b c, one of them is small, one is medium and one is large.
      *
-     * Return true if the three values are evenly spaced,
+     * Return true if the three values are evennly spaced,
      * so the difference between small and medium is the same as the difference between medium and large.
      *
      * evenlySpaced(2, 4, 6) => true
