7964fb53f171676044544f676c3987705b3227cd
heohak <heohak@ttu.ee>
2023-05-30 14:15:25 +0300

    harjutamine

--- TK/TK1/src/ee/taltech/iti0202/tk/art/Collector.java
+++ TK/TK1/src/ee/taltech/iti0202/tk/art/Collector.java
@@ -6,45 +6,34 @@ import java.util.List;
 
 public class Collector {
 
-    private List<Painting> paintings = new ArrayList<>();
+    List<Painting> paintings = new ArrayList<>();
 
-    /**
-     *
-     * @param painting
-     * @return boolean
-     */
-    public boolean addPainting(Painting painting) {
-        if (!paintings.contains(painting)) {
-            for (Painting p : paintings) {
-                if (p.getTitle().equals(painting.getTitle())) {
-                    return false;
-                }
+    boolean addPainting(Painting painting) {
+        if (paintings.contains(painting)) {
+            return false;
+        }
+        for (Painting painting1 : paintings) {
+            if (painting1.getTitle().equals(painting.getTitle())) {
+                return false;
             }
-            paintings.add(painting);
-            return true;
         }
-        return false;
+        paintings.add(painting);
+        return true;
     }
 
-    /**
-     *
-     * @param painting
-     * @param fellowCollector
-     * @return boolean
-     */
-    public boolean sellPainting(Painting painting, Collector fellowCollector) {
-        if (paintings.contains(painting) && fellowCollector != this && fellowCollector.addPainting(painting)) {
-            paintings.remove(painting);
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     *
-     * @return List
-     */
     public List<Painting> getPaintings() {
         return paintings;
     }
+
+    boolean sellPainting(Painting painting, Collector fellowCollector) {
+        if (paintings.contains(painting) && !fellowCollector.paintings.contains(painting)) {
+            if (fellowCollector.addPainting(painting)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
 }
--- TK/TK1/src/ee/taltech/iti0202/tk/art/Painting.java
+++ TK/TK1/src/ee/taltech/iti0202/tk/art/Painting.java
@@ -2,67 +2,41 @@ package ee.taltech.iti0202.tk.art;
 
 public class Painting {
     private String title;
+
     private String author;
 
-    /**
-     *
-     * @param title
-     */
     public Painting(String title) {
         this.title = title;
     }
 
-    /**
-     *
-     * @param title
-     * @param author
-     */
     public Painting(String title, String author) {
         this.title = title;
         this.author = author;
     }
 
-    /**
-     *
-     * @return String
-     */
     public String getTitle() {
         return title;
     }
 
-    /**
-     *
-     * @return String
-     */
-    public String getAuthor() {
-        return author;
-    }
-
-    /**
-     *
-     * @param title
-     */
     public void setTitle(String title) {
         this.title = title;
     }
 
-    /**
-     *
-     * @param author
-     */
+    public String getAuthor() {
+        return author;
+    }
+
     public void setAuthor(String author) {
         this.author = author;
     }
 
-    /**
-     *
-     * @return String
-     */
+    @Override
     public String toString() {
-        if (author != null) {
-            return "This is a painting named " + title + " and made by " + author + ".";
-        } else {
+        if (author == null) {
             return "This is a painting named " + title + " and made by an unknown artist.";
+        }
+        else {
+            return "This is a painting named " + title + " and made by " + author;
         }
     }
 }
